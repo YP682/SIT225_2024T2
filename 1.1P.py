7490cfcb6c8394764fe03c9e8b832466d3d566e9
unknown <ypokia07@gmail.com>
2024-07-17 17:59:26 +1000

    Initial commit: Added Python script and Arduino sketch for Week 1

---
+++ 1.1P.py
@@ -0,0 +1,35 @@
+import serial
+import random
+import time
+from datetime import datetime
+
+# Set baud rate, same speed as set in your Arduino sketch.
+baud_rate = 9600
+
+# Set serial port as suits your operating system
+s = serial.Serial('COM5', baud_rate, timeout=5)
+
+def log(message):
+    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - {message}")
+
+while True:  # Infinite loop, keep running
+    # A random number between 1 and 5.
+    data_send = random.randint(1, 5)
+
+    # Write to serial port, set data encoding.
+    d = s.write(bytes(str(data_send) + '\n', 'utf-8'))
+    log(f"Send >>> {data_send} ({d} bytes)")
+
+    # Read from serial port.
+    while True:
+        d = s.readline().decode("utf-8").strip()
+        if d == "DONE":
+            log("Arduino finished its task.")
+            break
+        if d.isdigit():
+            wait_time = int(d)
+            log(f"Received <<< {wait_time}")
+
+            log(f"Sleeping for {wait_time} seconds")
+            time.sleep(wait_time)
+            log("Sleeping done")
